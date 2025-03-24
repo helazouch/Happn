caf0ffd1a4385517ded684407450bead28f4664a
Ayyoub <ayyoub.mkadmi3@gmail.com>
2025-03-25 00:51:24 +0100

    cssAddEvent3

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,11 @@ import "./index.css";
 import ClientFile from "./PresentationLayer/admin/ClientFile";
 import EventsPage from "./PresentationLayer/admin/EventsPage";
 import AddEvent3 from "./PresentationLayer/admin/AddEvent3";
+import AddEvent1 from "./PresentationLayer/admin/AddEvent1";
+import AddEvent21 from "./PresentationLayer/admin/AddEvent21";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     {/* <EventsPage></EventsPage> */}
-  <AddEvent3></AddEvent3>
+    <AddEvent3></AddEvent3>
   </React.StrictMode>
 );
