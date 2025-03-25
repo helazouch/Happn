e969c27d5a0670c422e65b9fed9ec8697ac89b3d
helazouch <107811551+helazouch@users.noreply.github.com>
2025-03-25 14:39:27 +0100

    test

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-
-import AddEvent3 from "./PresentationLayer/admin/AddEvent3";
 import Login from "./PresentationLayer/participant/Login/Login";
-import ParticipantProfile from "./PresentationLayer/participant/ParticipantProfile/ParticipantProfile";
 import SignUp from "./PresentationLayer/participant/Login/SignUp/SignUp";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
