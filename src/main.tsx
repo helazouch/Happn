ee58d660e537d3962450b0616ac9fe2a46e1338d
arfaoui-yassine <72576669+arfaoui-yassine@users.noreply.github.com>
2025-03-25 14:49:05 +0100

    Landing page first

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./main.css";
 
 import AddEvent3 from "./PresentationLayer/admin/AddEvent3";
 import Login from "./PresentationLayer/participant/Login/Login";
 import ParticipantProfile from "./PresentationLayer/participant/ParticipantProfile/ParticipantProfile";
 import SignUp from "./PresentationLayer/participant/Login/SignUp/SignUp";
+import LandingPage from "./PresentationLayer/LandingPage/LandingPage";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     {/* <EventsPage></EventsPage> */}
-    <SignUp></SignUp>
-    <Login></Login>
+    <LandingPage></LandingPage>
   </React.StrictMode>
 );
